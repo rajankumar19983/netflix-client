f77ab30c6378e739eaa28225b69a3754b487dcf3
Rajan <rajankumar19983@gmail.com>
2025-04-10 23:29:24 +0530

    env fix

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: `${import.meta.env.VITE_SERVER_ADDRESS}/api`,
   timeout: 10000,
 });
--- src/utils/getLocalStream.js
+++
@@ -1,6 +0,0 @@
-export const getLocalStream = async () => {
-  return await navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: true,
-  });
-};
